51f484ed92fc9e4a4d3940df342c837ee96a2a01
ajvillav <ajvillav@unal.edu.co>
2021-04-02 20:59:37 -0500

    validate class deleted and optmization of the code

--- src/login/TestLogin.java
+++ src/login/TestLogin.java
@@ -7,11 +7,10 @@ public class TestLogin {
     public static void main(String[] args) {
 
         Scanner consola = new Scanner(System.in);
-        CreateEmail validate = new CreateEmail();
-        ValidarCorreo validate2 = new ValidarCorreo();
+        User validate = new User();
 
         System.out.println("Now we are going to create and login a mail");
-        validate.test();
+        validate.createEmail();
 
         while (true) {
             System.out.println("Please type your email:");
@@ -20,20 +19,8 @@ public class TestLogin {
             System.out.println("Please type your password:");
             String pass = consola.nextLine();
 
-            if (validate.email.equals(email)) {
-                validate2.email = true;
-            } else {
-                validate2.email = false;
-            }
-
-            if (validate.pass.equals(pass)) {
-                validate2.pass = true;
-            } else {
-                validate2.pass = false;
-            }
-
             if (validate.email.equals(email) && validate.pass.equals(pass)) {
-                validate2.validarInfo(validate2.email, validate2.pass);
+                System.out.println("You have logged in correctly");
                 break;
             } else {
                 System.out.println("email or password wrong, try again.");
